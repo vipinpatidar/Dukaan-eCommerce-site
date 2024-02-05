3321be8aca591e0d78b6a8fc7bbc9d6f11d1e235
vipin Patidar <vipinpatidar5@gmail.com>
2024-02-05 18:26:59 +0530

    added frontend link to cors

--- api/app.js
+++ api/app.js
@@ -24,7 +24,10 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://dukaan-ecommerce-vipin.netlify.app",
+    ],
     credentials: true,
   })
 );
